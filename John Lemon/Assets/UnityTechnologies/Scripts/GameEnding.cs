e3851951b04cc08e6b177f8988a1b685d1a28f07
OngoingZebra143 <146136142+OngoingZebra143@users.noreply.github.com>
2024-01-23 14:25:44 -0800

    left off at part 2

--- John Lemon/Assets/UnityTechnologies/Scripts/GameEnding.cs
+++ John Lemon/Assets/UnityTechnologies/Scripts/GameEnding.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
@@ -8,25 +9,39 @@ public class GameEnding : MonoBehaviour
     public float displayImageDuration = 1f;
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
     bool m_IsplayerAtExit;
     float m_Timer;
+    bool m_IsPlayerCaught;
+
 
     // Update is called once per frame
     void Update()
     {
         if(m_IsplayerAtExit)
         {
-            EndLevel();
+            EndLevel (exitBackgroundImageCanvasGroup, false);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel (caughtBackgroundImageCanvasGroup, true);
         }
     }
-    void EndLevel()
+    void EndLevel(CanvasGroup imageCanvasGroup,bool doRestart)
     {
        m_Timer+= Time.deltaTime;
-       exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+       imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
-       if(m_Timer > fadeDuration + displayImageDuration);
+        if(m_Timer > fadeDuration + displayImageDuration);
        {
-        Application.Quit();
+            if (doRestart)
+            {
+                SceneManager.LoadScene (0);
+            }
+            else
+            {
+                Application.Quit();
+            }
        }
     }
 
@@ -36,5 +51,10 @@ public class GameEnding : MonoBehaviour
         {
             m_IsplayerAtExit = true;
         }
+    }
+
+    public void CaughtPlayer ()
+    {
+        m_IsPlayerCaught = true;
     }
 }
